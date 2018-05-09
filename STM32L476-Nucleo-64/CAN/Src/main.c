48eed821338a4bb17c73700eadd16d67b6c4ea5f
arao <araobp@users.noreply.github.com>
2018-05-09 21:41:35 +0900

    update

--- STM32L476-Nucleo-64/CAN/Src/main.c
+++ STM32L476-Nucleo-64/CAN/Src/main.c
@@ -51,7 +51,12 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-
+#define STD_ID1 0x0001  // CAN Standard Identifier 1
+#define STD_ID2 0x0002  // CAN Standard Identifier 2
+#define FILTER_ID1 0x0001  // Filter 1
+#define FILTER_MASK1 0x003F  // Mask 1
+#define FILTER_ID2 0x0002  // Filter 2
+#define FILTER_MASK2 0x003F  // Mask 2
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -66,7 +71,11 @@ static void MX_CAN1_Init(void);
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
-
+// printf redirect
+int _write(int file, char *pbuf, int len) {
+  HAL_UART_Transmit(&huart2, (uint8_t *)pbuf, len, 1000);
+  return len;
+}
 /* USER CODE END 0 */
 
 /**
@@ -77,6 +86,60 @@ static void MX_CAN1_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+  uint8_t payload1[] = "Hello!\0";  // Payload for StdID 1
+  uint8_t payload2[] = "World!\0";  // Payload for StdID 2
+
+  uint8_t RxData[8] = { 0 };
+  CAN_RxHeaderTypeDef RxHeader;
+
+  uint32_t TxMailbox1;
+  uint32_t TxMailbox2;
+
+  CAN_TxHeaderTypeDef header1;
+  header1.StdId = STD_ID1;
+  header1.ExtId = 0x0000;
+  header1.DLC = sizeof(payload1);
+  header1.RTR = CAN_RTR_DATA;
+  header1.IDE = CAN_ID_STD;
+  header1.TransmitGlobalTime = DISABLE;
+
+  CAN_TxHeaderTypeDef header2;
+  header2.StdId = STD_ID2;
+  header2.ExtId = 0x0000;
+  header2.DLC = sizeof(payload2);
+  header2.RTR = CAN_RTR_DATA;
+  header2.IDE = CAN_ID_STD;
+  header2.TransmitGlobalTime = DISABLE;
+
+  /*
+   * Filter setting for Filter Bank 0
+   * STM32L4 reference manual P1657
+   */
+  CAN_FilterTypeDef filterConf0;
+  filterConf0.FilterBank = 0;
+  filterConf0.FilterMode = CAN_FILTERMODE_IDMASK;
+  filterConf0.FilterScale = CAN_FILTERSCALE_32BIT;
+  filterConf0.FilterIdHigh = FILTER_ID1 << 5;
+  filterConf0.FilterIdLow = 0x0000;
+  filterConf0.FilterMaskIdHigh = FILTER_MASK1 << 5;
+  filterConf0.FilterMaskIdLow = 0x0000;
+  filterConf0.FilterFIFOAssignment = CAN_RX_FIFO0;
+  filterConf0.FilterActivation = ENABLE;
+
+  /*
+   * Filter setting for Filter Bank 1
+   * STM32L4 reference manual P1657
+   */
+  CAN_FilterTypeDef filterConf1;
+  filterConf1.FilterBank = 1;
+  filterConf1.FilterMode = CAN_FILTERMODE_IDMASK;
+  filterConf1.FilterScale = CAN_FILTERSCALE_32BIT;
+  filterConf1.FilterIdHigh = FILTER_ID2 << 5;
+  filterConf1.FilterIdLow = 0x0000;
+  filterConf1.FilterMaskIdHigh = FILTER_MASK2 << 5;
+  filterConf1.FilterMaskIdLow = 0x0000;
+  filterConf1.FilterFIFOAssignment = CAN_RX_FIFO1;
+  filterConf1.FilterActivation = ENABLE;
 
   /* USER CODE END 1 */
 
@@ -93,7 +156,6 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
@@ -101,13 +163,72 @@ int main(void)
   MX_USART2_UART_Init();
   MX_CAN1_Init();
   /* USER CODE BEGIN 2 */
+  if (HAL_CAN_ConfigFilter(&hcan1, &filterConf0) != HAL_OK) {
+    Error_Handler();
+  }
+  if (HAL_CAN_ConfigFilter(&hcan1, &filterConf1) != HAL_OK) {
+    Error_Handler();
+  }
 
+  if (HAL_CAN_Start(&hcan1) != HAL_OK) {
+    Error_Handler();
+  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+    // Transmit message
+    if (HAL_CAN_AddTxMessage(&hcan1, &header1, payload1, &TxMailbox1) != HAL_OK) {
+      Error_Handler();
+    }
+    printf("\n[1] MailBox#: %d\n", (int)TxMailbox1);
+
+    // Wait for all the three mailboxes to be free
+    while(HAL_CAN_GetTxMailboxesFreeLevel(&hcan1) != 3) {
+      HAL_Delay(8);
+    }
+
+    if (HAL_CAN_AddTxMessage(&hcan1, &header2, payload2, &TxMailbox2) != HAL_OK) {
+      Error_Handler();
+    }
+    printf("[2] MailBox#: %d\n", (int)TxMailbox2);
+
+    // Wait for all the three mailboxes to be free
+    while(HAL_CAN_GetTxMailboxesFreeLevel(&hcan1) != 3) {
+      HAL_Delay(8);
+    }
+
+    // Wait for incoming message to be filled
+    while(HAL_CAN_GetRxFifoFillLevel(&hcan1, CAN_RX_FIFO0) == 0) {
+      HAL_Delay(8);
+    }
+
+    // Get the incoming message
+    if(HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK) {
+      Error_Handler();
+    } else {
+      printf("[1] StdId: %d\n", (int)RxHeader.StdId);
+      printf("[1] RX_FIFO0: %s\n", RxData);
+    }
+
+    // Wait for incoming message to be filled
+    while(HAL_CAN_GetRxFifoFillLevel(&hcan1, CAN_RX_FIFO1) == 0) {
+      HAL_Delay(8);
+    }
+
+    // Get the incoming message
+    if(HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO1, &RxHeader, RxData) != HAL_OK) {
+      Error_Handler();
+    } else {
+      printf("[2] StdId: %d\n", (int)RxHeader.StdId);
+      printf("[2] RX_FIFO1: %s\n", RxData);
+    }
+
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    HAL_Delay(1000);
 
   /* USER CODE END WHILE */
 
@@ -190,15 +311,15 @@ static void MX_CAN1_Init(void)
 {
 
   hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 16;
-  hcan1.Init.Mode = CAN_MODE_LOOPBACK;
+  hcan1.Init.Prescaler = 40;
+  hcan1.Init.Mode = CAN_MODE_SILENT_LOOPBACK;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_3TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_3TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_10TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_6TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
   hcan1.Init.AutoBusOff = DISABLE;
   hcan1.Init.AutoWakeUp = DISABLE;
-  hcan1.Init.AutoRetransmission = DISABLE;
+  hcan1.Init.AutoRetransmission = ENABLE;
   hcan1.Init.ReceiveFifoLocked = DISABLE;
   hcan1.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan1) != HAL_OK)
@@ -266,7 +387,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
 /* USER CODE END 4 */
 
 /**
