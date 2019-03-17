ca3304d25d1a9e0e5b9e1dd2da030b64be694d6b
arao <araobp@users.noreply.github.com>
2019-03-17 21:41:34 +0900

    FTDI driver test

---
+++ Android/FTDI/app/src/main/java/jp/araobp/ai/ftdi/MainActivity.kt
@@ -0,0 +1,35 @@
+package jp.araobp.ai.ftdi
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import jp.araobp.serial.Message
+import jp.araobp.serial.jp.araobp.serial.service.FtdiDriver
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = this::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Register this activity with EventBus
+        EventBus.getDefault().register(this)
+
+        // Start SerialService
+        val intent = Intent(application, FtdiDriver::class.java)
+        startService(intent)
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessage(message: Message) {
+        Log.d(TAG, message.toString())
+    }
+}
---
+++ Android/FTDI/app/src/main/java/jp/araobp/serial/Message.kt
@@ -0,0 +1,3 @@
+package jp.araobp.serial
+
+data class Message(var message: String = "")
